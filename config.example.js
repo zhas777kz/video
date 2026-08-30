40ba09447c26470942c0fb4d41148f7a0da76215
zhas777kz <zhas777kz@gmail.com>
2026-08-30 23:09:34 +0500

    Delete config.example.js

--- config.example.js
+++
@@ -1,7 +0,0 @@
-window.APP_CONFIG = {
-  siteName: "ВидеоКласс",
-  supabaseUrl: "https://YOUR_PROJECT.supabase.co",
-  supabaseAnonKey: "YOUR_PUBLIC_ANON_KEY",
-  storageBucket: "lesson-videos",
-  completionPercent: 90
-};
